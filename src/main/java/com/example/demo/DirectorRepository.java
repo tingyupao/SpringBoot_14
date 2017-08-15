c8a52aa918fbb71f2701dd2f661fec057eb69cb0
Tingyu Pao <paotingyu@gmail.com>
2017-08-14 21:02:18 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DirectorRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by oracle on 8/14/17.
+ */
+public interface DirectorRepository extends CrudRepository<Director,Long> {
+}
